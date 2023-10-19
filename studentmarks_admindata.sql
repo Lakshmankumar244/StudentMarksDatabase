a54540248adc9f5429909d62d4eff79a9ac1dea4
Lakshmankumar244 <141116386+Lakshmankumar244@users.noreply.github.com>
2023-10-19 16:52:09 +0530

    student marks dot  sql extension

---
+++ studentmarks_admindata.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: studentmarks
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admindata`
+--
+
+DROP TABLE IF EXISTS `admindata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admindata` (
+  `aid` bigint NOT NULL,
+  `password` varchar(45) NOT NULL,
+  UNIQUE KEY `password_UNIQUE` (`password`),
+  UNIQUE KEY `aid_UNIQUE` (`aid`),
+  UNIQUE KEY `aid` (`aid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admindata`
+--
+
+LOCK TABLES `admindata` WRITE;
+/*!40000 ALTER TABLE `admindata` DISABLE KEYS */;
+INSERT INTO `admindata` VALUES (1001000100,'admin@abk'),(1001000101,'admin#abk'),(1001000102,'admin$abk'),(1001000103,'admin*abk'),(1001000104,'admin!abk'),(1001001001,'admin@@abk'),(1001100100,'admin**abk'),(1001991991,'admin//abk');
+/*!40000 ALTER TABLE `admindata` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-19 16:31:22
